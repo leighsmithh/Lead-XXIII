5105811e8bd9733389a78451f8478cd43b1e2d99
leighsmithh <70735907+leighsmithh@users.noreply.github.com>
2023-08-28 22:56:19 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 console.log("Script starting...");
-const AdminBro = require('admin-bro');
-const AdminBroExpress = require('@admin-bro/express');
-const express = require('express');
+const AdminBro = require('admin-bro')
+const AdminBroExpress = require('@admin-bro/express')
+const express = require('express')
 
 const adminBro = new AdminBro({
-  rootPath: '/admin',
-});
+  rootPath: '/admin'
+})
 
-const app = express();
-const router = AdminBroExpress.buildRouter(adminBro);
+const app = express()
+const router = AdminBroExpress.buildRouter(adminBro)
 
-app.use(adminBro.options.rootPath, router);
-app.listen(8080, () => console.log('AdminBro is under localhost:8080/admin'));
+app.use(adminBro.options.rootPath, router)
+app.listen(8080, () => console.log('AdminBro is under localhost:8080/admin'))
+
 console.log("Script ended.");
 
